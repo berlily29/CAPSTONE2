2cbc0d4fd05756b6edf2baa0f203463268a1494c
Jaira <123538601+jrnng@users.noreply.github.com>
2025-01-16 14:05:34 +0800

    change layout of manage and pending

--- resources/views/admin/manage-events/view.blade.php
+++ resources/views/admin/manage-events/view.blade.php
@@ -1,49 +1,46 @@
 <x-app-layout>
-<div class="py-5">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="mt-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+<div class="pr-4">
+    <div class="p-8 bg-gray-50 border-b border-gray-200 min-h-screen">
+    
+    <div class="mb-3 pl-2">
+        <h1 class="text-lg text-gray-800">Manage</h1>
+        <h1 class="text-3xl font-bold text-gray-800">Events</h1>        
+    </div>
 
-        <div class="pt-10 pl-10">
-            <h1 class="text-lg text-black">Manage</h1>
-            <h1 class="text-3xl font-bold text-black">Events</h1>        
-        </div>
-
-        <div class="py-3 px-8  bg-white border-b border-gray-200">
-        
-        <div class="flex border-b mb-4 relative">
-            <button id="active-tab" class="px-3 py-2 font-medium border-b-2 focus:outline-none border-transparent text-black"
-                onclick="showTab('active')">
+    <div class="flex border-b mb-4 relative">
+        <div class="nav-tabs flex items-center space-x-4">
+            <button id="active-tab" class="px-2 py-2 font-medium border-b-2 focus:outline-none border-transparent text-black" onclick="showTab('active')">
                 Active
             </button>
             
-            <button id="post-events-tab" class="px-4 py-2 font-medium border-b-2 focus:outline-none border-transparent text-black"
-                onclick="showTab('post-events')">
+            <button id="post-events-tab" class="text-left  px-4 py-1 font-medium border-b-2 focus:outline-none border-transparent text-black" onclick="showTab('post-events')">
                 Post Events
             </button>
+        </div>
 
-            <div class="flex justify-end w-4/5">
-                <button id="" class="bg-pink-500 text-white py-2 px-3 rounded-2xl mb-3 flex justify-center hover:bg-pink-600">
-                    <span class="flex material-icons mr-3">schedule</span>
-                    Create Event
-                </button>
-            </div>
+        <div class="flex justify-end w-3/4">
+            <button id="" class="bg-pink-500 text-white py-2 px-2 rounded-2xl mb-3 flex justify-center hover:bg-pink-600">
+                <span class="flex material-icons mr-2">schedule</span>
+                Create Event
+            </button>
+        </div>
 
-            <div id="tab-highlight" class="absolute bottom-0 left-0 w-1/3 h-1 bg-pink-500 transition-all"></div>
-            </div>
 
-                <!--Active-->
-                <div id="active" class="tab-content hidden px-4">
-                    <h2 class="text-lg font-semibold text-gray-700">Active Events</h2>
-                </div>
+        <div id="tab-highlight" class="absolute bottom-0 left-0 w-1/3 h-1 bg-pink-500 transition-all"></div>
+        </div>
 
-                <!-- Post events -->
-                <div id="post-events" class="tab-content hidden px-4">
-                    <h2 class="text-lg font-semibold text-gray-700">Post Events</h2>
-                </div>
-                </div>
-            </div>
+        <!--Active-->
+        <div id="active" class="tab-content hidden px-4">
+            <h2 class="text-lg font-semibold text-gray-700">Active Events</h2>
+        </div>
+
+        <!-- Post events -->
+        <div id="post-events" class="tab-content hidden px-4">
+            <h2 class="text-lg font-semibold text-gray-700">Post Events</h2>
+        </div>
         </div>
     </div>
+</div>
 
     <script>
         function showTab(tabName) {
@@ -79,5 +76,4 @@
             showTab('active');
         });
     </script>
-
 </x-app-layout>
--- resources/views/admin/pending-request/view.blade.php
+++ resources/views/admin/pending-request/view.blade.php
@@ -1,42 +1,38 @@
 <x-app-layout>
-<div class="py-5">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="mt-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-
-        <div class="pt-10 pl-10">
-            <h1 class="text-lg text-black">Manage</h1>
-            <h1 class="text-3xl font-bold text-black">Pending Requests</h1>        
-        </div>
-
-        <div class="py-3 px-8  bg-white border-b border-gray-200">
-
-        <div class="flex border-b mb-4 relative">
-            <button id="users-tab" class="px-3 py-2 font-medium border-b-2 focus:outline-none border-transparent text-black"
-                onclick="showTab('users')">
+<div class="pr-4">
+    <div class="p-8 bg-gray-50 border-b border-gray-200 min-h-screen">
+    
+    <div class="mb-3 pl-2">
+        <h1 class="text-lg text-gray-800">Manage</h1>
+        <h1 class="text-3xl font-bold text-gray-800">Pending Requests</h1>        
+    </div>
+        
+    <div class="flex border-b mb-4 relative">
+        <div class="nav-tabs flex items-center space-x-4">
+            <button id="users-tab" class="px-2 py-2 font-medium border-b-2 focus:outline-none border-transparent text-black" onclick="showTab('users')">
                 Users
             </button>
             
-            <button id="events-tab" class="px-4 py-2 font-medium border-b-2 focus:outline-none border-transparent text-black"
-                onclick="showTab('events')">
-                Events
+            <button id="events-tab" class="px-4 py-1 font-medium border-b-2 focus:outline-none border-transparent text-black" onclick="showTab('events')">
+                 Events
             </button>
+        </div>
 
-            <div id="tab-highlight" class="absolute bottom-0 left-0 w-1/3 h-1 bg-pink-500 transition-all"></div>
-            </div>
-            
-                <!--Active-->
-                <div id="users" class="tab-content hidden px-4">
-                    <h2 class="text-lg font-semibold text-gray-700">Active Request</h2>
-                </div>
+        <div id="tab-highlight" class="absolute bottom-0 left-0 w-1/3 h-1 bg-pink-500 transition-all"></div>
+        </div>
 
-                <!-- Post events -->
-                <div id="events" class="tab-content hidden px-4">
-                    <h2 class="text-lg font-semibold text-gray-700">Events Request</h2>
-                </div>
-            </div>
-            </div>
+        <!--Active-->
+        <div id="users" class="tab-content hidden px-4">
+            <h2 class="text-lg font-semibold text-gray-700">Users Request</h2>
+        </div>
+
+        <!-- Post events -->
+        <div id="events" class="tab-content hidden px-4">
+            <h2 class="text-lg font-semibold text-gray-700"> Events Request</h2>
+        </div>
         </div>
     </div>
+</div>
 
     <script>
         function showTab(tabName) {
