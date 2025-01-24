e36a948495c14277ca74e4fc71dd8b891558be10
DEYYLLLL <123535361+bedaniadale@users.noreply.github.com>
2025-01-25 02:37:42 +0800

    Channel List UI

--- resources/views/organizer/channels/view.blade.php
+++ resources/views/organizer/channels/view.blade.php
@@ -5,8 +5,17 @@
             <h1 class="text-sm text-gray-500">Channels</h1>
             <h1 class="text-3xl font-black text-gray-700 mb-6">My Events</h1>
 
+            @if($events->count()==0)
+                <div class="w-full py-[4rem] flex items-center justify-center">
+                    <div class="border border-gray-400 flex flex-col items-center justify-center p-[5rem] rounded-xl text-gray-700 hover:border-gray-600 transition duration-300 ease-in-out hover:scale-105 gap-2">
+                        <!-- <span class="material-icons text-[10rem]">event_busy</span> -->
+                         <img src="{{asset('images/icons/unavailable.png')}}" alt="">
+                        <h1 class="">No Events Available</h1>
+                    </div>
+                </div>
+            @endif
             <!-- Event Cards Section -->
-            <div class="grid grid-cols-3 gap-8">
+            <div class="w-full grid grid-cols-3 gap-8">
                 @foreach($events as $event)
                     <div class="bg-white border border-gray-200 hover:border-gray-300 overflow-hidden transition duration-400 ease-in-out transform hover:scale-101">
 
