6891da3563344e29cd88211e1532c7bd482a46de
DEYYLLLL <123535361+bedaniadale@users.noreply.github.com>
2025-02-05 18:59:10 +0800

    [QA] Dashboard Details + IsApprove status disable recommendation

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -94,7 +94,7 @@ $config = \App\Models\AppConfig::find(1);
 
 
             <div class="space-y-1">
-              <label class="text-sm font-medium text-gray-600">Email</label>
+              <label class="text-sm font-medium text-gray-600">Password</label>
               <input type ="password" name="password" class="w-full px-3 py-2 border rounded-md focus:ring-2 focus:ring-pink-300 focus:border-pink-400">
 
               </select>
